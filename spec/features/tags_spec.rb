1c1315bdad4f01adcce71a86162bd913ee0d0449
Steven Chang <44954886+steven-chang-maestrano@users.noreply.github.com>
2019-05-04 22:06:26 +1000

    Refactor tags spec (#25)

--- spec/features/tags_spec.rb
+++ spec/features/tags_spec.rb
@@ -3,15 +3,10 @@
 require "rails_helper"
 
 RSpec.describe "tags", type: :feature, js: true do
-	let!(:post) { create(:post) }
 	let(:tag_name) { "horses" }
+	let!(:post_with_tag) { create(:post, tag_list: [tag_name]) }
 	let(:tag_name_two) { "on" }
 	let(:post_two) { create(:post) }
-	before do
-		post.tag_list.add(tag_name)
-		post.save
-		post.reload
-	end
 
 	describe "tags show page" do
 		before do
@@ -24,7 +19,7 @@ RSpec.describe "tags", type: :feature, js: true do
 		  end
 
 		  it "lists posts, on the tag's page with the title as a link" do
-		  	expect(page).to have_link post.title, href: post_path(post)
+		  	expect(page).to have_link post_with_tag.title, href: post_path(post_with_tag)
 		  end
 
 		  it "doesn't list unrelated posts on the tag's page" do
@@ -56,17 +51,17 @@ RSpec.describe "tags", type: :feature, js: true do
 
 	describe "post page" do
 	  it "displays a posts tags as a link" do
-	  	post.tag_list.add(tag_name_two)
-	  	post.save
-	  	visit post_path(post)
+	  	post_with_tag.tag_list.add(tag_name_two)
+	  	post_with_tag.save
+	  	visit post_path(post_with_tag)
 			expect(page).to have_link(tag_name, href: tag_path(name: tag_name))
 			expect(page).to have_link(tag_name_two, href: tag_path(name: tag_name_two))
 	  end
 
 		it "doesn't list unrelated tags for post on post page" do
 			post_two.tag_list.add(tag_name_two)
-			post.save
-			visit post_path(post)
+			post_two.save
+			visit post_path(post_with_tag)
 			expect(page)
 			  .not_to have_link(tag_name_two, href: tag_path(name: tag_name_two))
 		end
@@ -74,7 +69,7 @@ RSpec.describe "tags", type: :feature, js: true do
 
 	describe "adding tags while creating post" do
 		it "allows author of post to add tags while creating post" do
-			login_as(post.author)
+			login_as(post_with_tag.author)
 	    click_link "New Post"
 	    fill_in "post[title]", with: "RSpec"
 	    fill_in "post[body]", with: "RSpec"
@@ -87,9 +82,9 @@ RSpec.describe "tags", type: :feature, js: true do
 
 	describe "changing tags while editing post" do
 		before do
-	    login_as(post.author)
+	    login_as(post_with_tag.author)
 
-	    visit post_path(post)
+	    visit post_path(post_with_tag)
 	    click_link "Edit"
 		end
 	  it "allows author of post to add tags" do
