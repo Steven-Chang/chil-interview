27d60d3d25130cc0a23dbcd59102805fc5fa387a
Steven Chang <44954886+steven-chang-maestrano@users.noreply.github.com>
2019-05-06 10:41:40 +1000

    Update comments styling for sub-comments. (#32)

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 module CommentsHelper
+	def comments_container_class(commentable)
+		container_class = ""
+		container_class += "mt-5" unless commentable.class == Comment
+		"#{container_class} pl-3 ml-3 border-left" if commentable.class == Comment
+	end
+
 	def comments_container_id(commentable)
 		"#{commentable.class.to_s.downcase}-#{commentable.id}-comments"
 	end
